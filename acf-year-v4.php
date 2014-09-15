f5e0f86de5635626973a3c9e76ca82c8a2c364c7
Stef Pause <stef@stefpause.com>
2014-09-15 15:52:38 +0100

    Merge and update the v4 field with the latest v4 template

--- acf-year-v4.php
+++ acf-year-v4.php
@@ -1,342 +1,208 @@
 <?php
-/*
-* Plugin Name: Advanced Custom Fields Year Field add-on
-* Plugin URI:  https://github.com/ashworthconsulting/acf-year-field
-* Description: The year field lets you add a select field to Advanced Custom Fields with pre-populated years as a list to choose from.
-* Author:      Will Ashworth
-* Author URI:  https://github.com/ashworthconsulting/acf-year-field
-* Version:     1.0.0
-* License:     GPL2
-* Text Domain: acf-year-field
-* Domain Path: /lang/
-*/
 
-if( !class_exists( 'ACF_Year_Field' ) && class_exists( 'acf_Field' ) ) :
+class acf_field_year extends acf_field {
 
-	class ACF_Year_Field extends acf_Field
-	{
-		// Number of years to go back
-		const YEAR_RANGE = 50;
-
-		// Selected Age
-		const AGE_LIMIT = 0;
-
-		/*--------------------------------------------------------------------------------------
-		*
-		*	Constructor
-		*	- This function is called when the field class is initalized on each page.
-		*	- Here you can add filters / actions and setup any other functionality for your field
-		*
-		*	@author Elliot Condon
-		*	@since 2.2.0
-		*
-		*-------------------------------------------------------------------------------------*/
-
-		function __construct($parent)
-		{
-			// do not delete!
-			parent::__construct($parent);
-
-			// set name / title
-			$this->name = 'year'; // variable name (no spaces / special characters / etc)
-			$this->title = __("Year",'acf'); // field label (Displayed in edit screens)
-
-		}
-
-
-		/*--------------------------------------------------------------------------------------
-		*
-		*	create_options
-		*	- this function is called from core/field_meta_box.php to create extra options
-		*	for your field
-		*
-		*	@params
-		*	- $key (int) - the $_POST obejct key required to save the options to the field
-		*	- $field (array) - the field object
-		*
-		*	@author Elliot Condon
-		*	@since 2.2.0
-		*
-		*-------------------------------------------------------------------------------------*/
-
-		function create_options($key, $field)
-		{
-			// defaults
-			$field['startYear'] = isset($field['startYear']) ? $field['startYear'] : date('Y');
-			$field['yearRange'] = isset($field['yearRange']) ? $field['yearRange'] : self::YEAR_RANGE;
-
-		?>
-	        <tr class="field_option field_option_<?php echo $this->name; ?>">
-	            <td class="label">
-	                <label><?php _e("Start Year",'acf'); ?></label>
-	            </td>
-	            <td>
-					<?php
-					$this->parent->create_field(array(
-						'type'	=>	'text',
-						'name'	=>	'fields['.$key.'][startYear]',
-						'value'	=>	$field['startYear'],
-					));
-					?>
-	            </td>
-	        </tr>
-	        <tr class="field_option field_option_<?php echo $this->name; ?>">
-	            <td class="label">
-	                <label><?php _e("Year Range",'acf'); ?></label>
-	            </td>
-	            <td>
-					<?php
-					$this->parent->create_field(array(
-						'type'	=>	'text',
-						'name'	=>	'fields['.$key.'][yearRange]',
-						'value'	=>	$field['yearRange'],
-					));
-					?>
-	            </td>
-	        </tr>
-		<?php
-		}
-
-
-		/*--------------------------------------------------------------------------------------
-		*
-		*	create_field
-		*	- this function is called on edit screens to produce the html for this field
-		*
-		*	@author Elliot Condon
-		*	@since 2.2.0
-		*
-		*-------------------------------------------------------------------------------------*/
-
-		function create_field($field)
-		{
-			// defaults
-			$field['startYear'] = isset($field['startYear']) ? $field['startYear'] : date('Y');
-			$field['yearRange'] = isset($field['yearRange']) ? $field['yearRange'] : self::YEAR_RANGE;
-
-			// Generate Options
-			$startYear = $field['startYear'];
-			$endYear = ( $startYear - $field['yearRange'] );
-			//$selectYear = ( $startYear - $field['ageLimit'] );
-			$years = range( $startYear, $endYear );
-
-			echo '<select id="' . $field['name'] . '" class="' . $field['class'] . '" name="' . $field['name'] . '">' . "\n";
-			foreach ( $years as $year ) {
-				$selected = "";
-				if( $year == $field['value'] ) {
-					$selected = " selected";
-				}
-				echo '<option value="' . $year . '"' . $selected . '>' . $year . '</option>' . "\n";
-			}
-			echo '</select>' . "\n";
-		}
-
-
-		/*--------------------------------------------------------------------------------------
-		*
-		*	admin_head
-		*	- this function is called in the admin_head of the edit screen where your field
-		*	is created. Use this function to create css and javascript to assist your
-		*	create_field() function.
-		*
-		*	@author Elliot Condon
-		*	@since 2.2.0
-		*
-		*-------------------------------------------------------------------------------------*/
-
-		function admin_head()
-		{
-		?>
-	        <style type="text/css">
-	            .acf_postbox .field select.year {
-	                width: 20%;
-	                margin-left: 5px;
-	            }
-	        </style>
-		<?php
-		}
-
-
-		/*--------------------------------------------------------------------------------------
-		*
-		*	update_value
-		*	- this function is called when saving a post object that your field is assigned to.
-		*	the function will pass through the 3 parameters for you to use.
-		*
-		*	@params
-		*	- $post_id (int) - usefull if you need to save extra data or manipulate the current
-		*	post object
-		*	- $field (array) - usefull if you need to manipulate the $value based on a field option
-		*	- $value (mixed) - the new value of your field.
-		*
-		*	@author Elliot Condon
-		*	@since 2.2.0
-		*
-		*-------------------------------------------------------------------------------------*/
-
-		function update_value($post_id, $field, $value)
-		{
-			// save value
-			parent::update_value($post_id, $field, $value);
-		}
-
-
-
-
-
-		/*--------------------------------------------------------------------------------------
-		*
-		*	get_value
-		*	- called from the edit page to get the value of your field. This function is useful
-		*	if your field needs to collect extra data for your create_field() function.
-		*
-		*	@params
-		*	- $post_id (int) - the post ID which your value is attached to
-		*	- $field (array) - the field object.
-		*
-		*	@author Elliot Condon
-		*	@since 2.2.0
-		*
-		*-------------------------------------------------------------------------------------*/
-
-		function get_value($post_id, $field)
-		{
-			// get value
-			$value = parent::get_value($post_id, $field);
-
-			// return value
-			return $value;
-		}
-
-
-		/*--------------------------------------------------------------------------------------
-		*
-		*	get_value_for_api
-		*	- called from your template file when using the API functions (get_field, etc).
-		*	This function is useful if your field needs to format the returned value
-		*
-		*	@params
-		*	- $post_id (int) - the post ID which your value is attached to
-		*	- $field (array) - the field object.
-		*
-		*	@author Elliot Condon
-		*	@since 3.0.0
-		*
-		*-------------------------------------------------------------------------------------*/
-
-		function get_value_for_api($post_id, $field)
-		{
-			// get value
-			$value = $this->get_value($post_id, $field);
-
-			// return value
-			return $value;
-
-		}
-
-	}
-
-endif; //class_exists 'ACF_Year_Field'
-
-
-if( !class_exists( 'ACF_Year_Field_Helper' ) ) :
+	var $settings, // will hold info such as dir / path
+	$defaults; // will hold default field options
 
 	/*
-	 * Advanced Custom Fields - Year Field Helper
-	 *
-	 * @author Brian Zoetewey <brian.zoetewey@ccci.org>
-	 *
-	 */
-	class ACF_Year_Field_Helper {
-		/*
-		 * Singleton instance
-		 * @var ACF_Year_Field_Helper
-		 *
-		 */
-		private static $instance;
+	*  __construct
+	*
+	*  Set name / label needed for actions / filters
+	*
+	*  @since	3.6
+	*  @date	23/01/13
+	*/
 
-		/*
-		 * Returns the ACF_Year_Field_Helper singleton
-		 *
-		 * <code>$obj = ACF_Year_Field_Helper::singleton();</code>
-		 * @return ACF_Year_Field_Helper
-		 *
-		 */
-		public static function singleton()
-		{
-			if( !isset( self::$instance ) )
-			{
-				$class = __CLASS__;
-				self::$instance = new $class();
-			}
-			return self::$instance;
-		}
+	function __construct()
+	{
+		// set name / title
+		$this->name = 'year'; // variable name (no spaces / special characters / etc)
+		$this->label = __('Year'); // field label (Displayed in edit screens)
+		$this->category = __('Choice', 'acf');
+		$this->defaults = array(
+			// add default here to merge into your field. 
+			// This makes life easy when creating the field options as you don't need to use any if( isset('') ) logic. eg:
+			//'preview_size' => 'thumbnail'
+			'year_range' => 20
+		);
 
-		/*
-		 * Prevent cloning of the ACF_Year_Field_Helper object
-		 * @internal
-		 *
-		 */
-		private function __clone()
-		{
+		
+		// do not delete!
+		parent::__construct();
 
-		}
+    	
+		// settings
+		$this->settings = array(
+			'path' => apply_filters('acf/helpers/get_path', __FILE__),
+			'dir' => apply_filters('acf/helpers/get_dir', __FILE__),
+			'version' => '1.0.0'
+		);
 
-		/*
-		* WordPress Localization Text Domain
-		*
-		* Used in wordpress localization and translation methods.
-		* @var string
-		*
-		*/
-		const L10N_DOMAIN = 'acf-year-field';
-
-		/*
-		 * Language directory path
-		 *
-		 * Used to build the path for WordPress localization files.
-		 * @var string
-		 *
-		 */
-		private $lang_dir;
-
-		/*
-		 * Constructor
-		 *
-		 */
-		private function __construct()
-		{
-			$this->lang_dir = rtrim( dirname( realpath( __FILE__ ) ), '/' ) . '/lang';
-
-			add_action( 'init', array( &$this, 'register_field' ),  5, 0 );
-			add_action( 'init', array( &$this, 'load_textdomain' ), 2, 0 );
-		}
-
-		/*
-		 * Registers the Field with Advanced Custom Fields
-		 *
-		 */
-		public function register_field()
-		{
-			if( function_exists( 'register_field' ) )
-			{
-				register_field( 'ACF_Year_Field', __FILE__ );
-			}
-		}
-
-		/*
-		 * Loads the textdomain for the current locale if it exists
-		 *
-		 */
-		public function load_textdomain()
-		{
-			$locale = get_locale();
-			$mofile = $this->lang_dir . '/' . self::L10N_DOMAIN . '-' . $locale . '.mo';
-			load_textdomain( self::L10N_DOMAIN, $mofile );
-		}
 	}
 
-endif; //class_exists 'ACF_Year_Field_Helper'
 
-//Instantiate the Addon Helper class
-ACF_Year_Field_Helper::singleton();
+	/*
+	*  create_options()
+	*
+	*  Create extra options for your field. This is rendered when editing a field.
+	*  The value of $field['name'] can be used (like below) to save extra data to the $field
+	*
+	*  @type	action
+	*  @since	3.6
+	*  @date	23/01/13
+	*
+	*  @param	$field	- an array holding all the field's data
+	*/
+
+	function create_options($field)
+	{
+		// defaults?
+		/*
+		$field = array_merge($this->defaults, $field);
+		*/
+		// defaults
+		$field['startYear'] = isset($field['startYear']) ? $field['startYear'] : date('Y');
+		$field['yearRange'] = isset($field['yearRange']) ? $field['yearRange'] : $this->defaults['year_range'];
+
+		// key is needed in the field names to correctly save the data
+		$key = $field['name'];
+
+		
+		// Create Field Options HTML
+	?>
+		<tr class="field_option field_option_<?php echo $this->name; ?>">
+			<td class="label">
+				<label><?php _e("Start Year",'acf'); ?></label>
+				<p class="description"><?php _e("Choose Year",'acf'); ?></p>
+			</td>
+			<td>
+				<?php
+		
+				do_action('acf/create_field', array(
+					'type'	=>	'text',
+					'name'	=>	'fields['.$key.'][startYear]',
+					'value'	=>	$field['startYear'],
+				));
+		
+				?>
+			</td>
+		</tr>
+		<tr class="field_option field_option_<?php echo $this->name; ?>">
+			<td class="label">
+				<label><?php _e("Year Range",'acf'); ?></label>
+			</td>
+			<td>
+				<?php
+		
+				do_action('acf/create_field', array(
+					'type'	=>	'text',
+					'name'	=>	'fields['.$key.'][yearRange]',
+					'value'	=>	$field['yearRange'],
+				));
+				?>
+			</td>
+		</tr>
+	<?php
+	}
+
+
+	/*
+	*  create_field()
+	*
+	*  Create the HTML interface for your field
+	*
+	*  @param	$field - an array holding all the field's data
+	*
+	*  @type	action
+	*  @since	3.6
+	*  @date	23/01/13
+	*/
+
+	function create_field($field)
+	{
+		// defaults?
+		/*
+		$field = array_merge($this->defaults, $field);
+		*/
+		// defaults
+		$field['startYear'] = isset($field['startYear']) ? $field['startYear'] : date('Y');
+		$field['yearRange'] = isset($field['yearRange']) ? $field['yearRange'] : $this->defaults['year_range'];
+
+		// perhaps use $field['preview_size'] to alter the markup?
+		// Generate Options
+		$startYear = $field['startYear'];
+		$endYear = ( $startYear + $field['yearRange'] );
+		//$selectYear = ( $startYear - $field['ageLimit'] );
+		$years = range( $startYear, $endYear );
+
+		
+		// create Field HTML
+		echo '<select id="' . $field['name'] . '" class="' . $field['class'] . '" name="' . $field['name'] . '">' . "\n";
+		foreach ( $years as $year ) {
+			$selected = "";
+			if( $year == $field['value'] ) {
+				$selected = " selected";
+			}
+			echo '<option value="' . $year . '"' . $selected . '>' . $year . '</option>' . "\n";
+		}
+		echo '</select>' . "\n";
+	}
+
+
+	/*
+	*  input_admin_head()
+	*
+	*  This action is called in the admin_head action on the edit screen where your field is created.
+	*  Use this action to add CSS and JavaScript to assist your create_field() action.
+	*
+	*  @info	http://codex.wordpress.org/Plugin_API/Action_Reference/admin_head
+	*  @type	action
+	*  @since	3.6
+	*  @date	23/01/13
+	*/
+
+	function input_admin_head()
+	{
+	?>
+		<style type="text/css">
+			.acf_postbox .field select.year {
+				width: 20%;
+				margin-left: 5px;
+			}
+		</style>
+	<?php
+	}
+
+
+	/*
+	*  update_value()
+	*
+	*  This filter is applied to the $value before it is updated in the db
+	*
+	*  @type	filter
+	*  @since	3.6
+	*  @date	23/01/13
+	*
+	*  @param	$value - the value which will be saved in the database
+	*  @param	$post_id - the $post_id of which the value will be saved
+	*  @param	$field - the field array holding all the field options
+	*
+	*  @return	$value - the modified value
+	*/
+
+	function update_value( $value, $post_id, $field )
+	{
+		// save value
+		return $value;
+	}
+
+
+
+}
+
+
+// create field
+new acf_field_year();
+
+?>
